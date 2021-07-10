c541f6fb7877f3d696f86420cee4c64e3a80a058
nemerof <nemerof716@gmail.com>
2021-07-10 11:46:43 +0300

    change to postgreSQL

--- src/main/java/com/training/vueblog/VueblogApplication.java
+++ src/main/java/com/training/vueblog/VueblogApplication.java
@@ -2,9 +2,7 @@ package com.training.vueblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories
 @SpringBootApplication
 public class VueblogApplication {
 
--- src/main/java/com/training/vueblog/objects/User.java
+++ src/main/java/com/training/vueblog/objects/User.java
@@ -2,18 +2,18 @@ package com.training.vueblog.objects;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 
 @Entity
-@Document(collection = "usr")
+@Table(name = "usr")
 @Getter
 @Setter
 public class User {
