b90ba026a62c677ebaa9e0cd9512b8b6034d4c49
Artyom <divinefridaay@mail.ru>
2021-07-15 18:41:40 +0300

    Added file as a message parameter

--- src/main/java/com/training/vueblog/controllers/MessageController.java
+++ src/main/java/com/training/vueblog/controllers/MessageController.java
@@ -5,6 +5,7 @@ import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.training.vueblog.objects.Message;
 import com.training.vueblog.repositories.MessageRepository;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/message")
@@ -51,14 +53,16 @@ public class MessageController {
         return messageRepository.findById(id).orElse(null);
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*") // не уверен что это нужно)
     @PostMapping("/add")
-    public Message addMessage(@RequestBody Message message) {
-        message.setId(UUID.randomUUID().toString());
-        message.setCreationDate(LocalDateTime.now());
+    public Message addMessage(@RequestPart("text") Message message, @RequestParam("file") MultipartFile file2)
+      throws IOException {
+      message.setId(UUID.randomUUID().toString());
+      message.setCreationDate(LocalDateTime.now());
 
-        messageRepository.save(message);
+      messageRepository.save(message);
 
-        return message;
+      return message;
     }
 
     @DeleteMapping("/{id}")
